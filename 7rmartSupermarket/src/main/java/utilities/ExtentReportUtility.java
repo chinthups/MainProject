85dbd0c91a6c563f97421d058f8b23d708d261e4
SaranyaSPillai <SaranyaSPillai@SMM-LP172>
2025-05-05 12:59:13 +0530

    Final project3

---
+++ 7rmartSupermarket/src/main/java/utilities/ExtentReportUtility.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportUtility {
+	
+	public static final ExtentReports extentReports = new ExtentReports();
+
+	public synchronized static ExtentReports createExtentReports() {
+
+		ExtentSparkReporter reporter = new ExtentSparkReporter("./extent-reports/extent-report.html");
+
+		reporter.config().setReportName("7Rmart Supermarket");
+
+		extentReports.attachReporter(reporter);
+
+		extentReports.setSystemInfo("Organization", "Obsqura");
+
+		extentReports.setSystemInfo("Name", " Saranya "); 
+
+		return extentReports;
+
+	}
+
+}
---
+++ 7rmartSupermarket/src/main/java/utilities/RandomDataUtility.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import com.github.javafaker.Faker;
+
+public class RandomDataUtility {
+	Faker faker=new Faker();
+	public String  createrandomusername()
+	{
+		return faker.name().username();
+	}
+	public String createrandompassword()
+	{
+		return faker.internet().password();
+	}
+	public String createrandomfullname()
+	{
+		return faker.name().fullName();
+	}
+	
+	public String createrandomemail()
+	{
+		return faker.internet().emailAddress();
+	}
+	public String createrandomfulladdress()
+	{
+		return faker.address().fullAddress();
+	}
+
+}
