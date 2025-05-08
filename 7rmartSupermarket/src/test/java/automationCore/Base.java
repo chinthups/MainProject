f600f91fe82a9f69aca0497244a260a02c16da6b
SaranyaSPillai <SaranyaSPillai@SMM-LP172>
2025-05-08 20:12:20 +0530

    New changes

--- 7rmartSupermarket/src/test/java/automationCore/Base.java
+++ 7rmartSupermarket/src/test/java/automationCore/Base.java
@@ -5,9 +5,14 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import com.beust.jcommander.Parameter;
 
 import utilities.ScreenShotUtlity;
 
@@ -15,9 +20,27 @@ public class Base {
 	
 	public WebDriver driver;
 	@BeforeMethod
-	public void initialiseBrowser()
+	@Parameters("browser")
+	public void initialiseBrowser(String browser) throws Exception
 	{
-		driver=new ChromeDriver();
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver=new ChromeDriver();
+			
+		}
+		else if(browser.equalsIgnoreCase("edge"))
+		{
+			driver=new EdgeDriver();
+			
+		}
+		else if(browser.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+			
+		}
+		else
+		{
+			throw new Exception("invalid browser");
+		}
 		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
--- 7rmartSupermarket/src/test/java/testscript/LoginTest.java
+++ 7rmartSupermarket/src/test/java/testscript/LoginTest.java
@@ -2,17 +2,20 @@ package testscript;
 
 import java.io.IOException;
 
+import org.openqa.selenium.devtools.Message;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import automationCore.Base;
+import constants.Messages;
 import pages.LoginPage;
 import utilities.Exelutility;
 
 
 
 public class LoginTest extends Base {
-	@Test
+	@Test(priority = 1,description = "Verify Whether the user is able to login with valid credentials",retryAnalyzer = retry.Retry.class)
 	public void verifyuserloginwithvalidcredentials() throws IOException
 	{
 	String 	username=Exelutility.readStringData(0, 0, "Loginpage");
@@ -22,10 +25,10 @@ public class LoginTest extends Base {
 	login.enterPasswordOnPasswordField(password);
 	login.clickONLoginButon();
 	boolean isdashboarddisplayed=login.dashboardDisplayed();
-	Assert.assertTrue(isdashboarddisplayed, "User was Unable to Login Successfully ");
+	Assert.assertTrue(isdashboarddisplayed,Messages.VALIDCRENDENTIALS);
 		
 	}
-	@Test
+	@Test(priority = 2,description = "Verify Whether the user is unable  to login with invalid username")
 	public void verifyuserloginwithinvalidusername() throws IOException
 	{
 		String 	username=Exelutility.readStringData(1, 0, "Loginpage");
@@ -40,7 +43,7 @@ public class LoginTest extends Base {
 			
 			
 	}
-	@Test
+	@Test(priority = 3,description = "Verify Whether the user is unable  to login with invalid password")
 	public void verifyuserloginwithinvalidpassword() throws IOException
 	{
 		String 	username=Exelutility.readStringData(2, 0, "Loginpage");
@@ -51,14 +54,22 @@ public class LoginTest extends Base {
 		login.clickONLoginButon();
 			
 	}
-	@Test
-	public void verifyuserloginwithinvalidpasswordandusername() throws IOException
+	@Test(priority = 4,description = "Verify Whether the user is unable  to login with invalid credentials",dataProvider ="loginProvider" )
+	public void verifyuserloginwithinvalidpasswordandusername(String username,String password ) throws IOException
 	{
-		String 	username=Exelutility.readStringData(3, 0, "Loginpage");
-		String password =Exelutility.readStringData(3, 1, "Loginpage");
+		//String 	username=Exelutility.readStringData(3, 0, "Loginpage");
+		//String password =Exelutility.readStringData(3, 1, "Loginpage");
 		LoginPage login=new LoginPage(driver);
 		login.enterUsernameOnUserNameField(username);
 		login.enterPasswordOnPasswordField(password);
 		login.clickONLoginButon();
 	}
+	@DataProvider(name="loginProvider")
+	public Object[][] getDataFromDataProvider() throws IOException
+	{
+		return new Object[][] { new Object[] {"admin","admin"},new Object[] {"123","1234"} 
+				
+					
+		};
+}
 }
